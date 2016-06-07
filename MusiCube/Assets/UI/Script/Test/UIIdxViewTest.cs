e4fa220e7f39e3e083c1f97f37c70a67ce9d7b68
abucraft <499817366@qq.com>
2016-06-07 11:37:11 +0800

    Add UIIdxView

---
+++ MusiCube/Assets/UI/Script/Test/UIIdxViewTest.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+namespace MusiCube
+{
+    namespace Test
+    {
+        [RequireComponent(typeof(UIIdxView))]
+        public class UIIdxViewTest : MonoBehaviour
+        {
+
+            void Start()
+            {
+                test(GetComponent<UIIdxView>());
+            }
+            public void test(UIIdxView view)
+            {
+                view.timestamps = new List<int>();
+                view.timestamps.Add(200);
+                view.notes = new SortedDictionary<int, List<Note>>();
+                view.notes[200] = new List<Note>();
+                view.notes[200].Add(new Note());
+                view.notes[200].Add(new Note());
+            }
+        }
+    }
+}

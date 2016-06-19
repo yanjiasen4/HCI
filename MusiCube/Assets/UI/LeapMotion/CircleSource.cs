3734756a62711ca23d4a787eee0693538f711942
Yang Ming <13122169550@163.com>
2016-06-19 16:00:14 +0800

    文件夹改动

---
+++ MusiCube/Assets/UI/LeapMotion/CircleSource.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CircleSource : MonoBehaviour {
+
+    private Sprite[] _circleSequence;
+    private static readonly CircleSource _instance = new CircleSource();
+    public static CircleSource instance { get { return _instance; } }
+    public Sprite[] circleSequence
+    {
+        get
+        {
+            if (_circleSequence == null)
+                _circleSequence = Resources.LoadAll<Sprite>("CircleSequence2");
+            return _circleSequence;
+        }
+    }
+    // Use this for initialization
+    void Start () {
+       
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ MusiCube/Assets/UI/LeapMotion/UIHand.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UIHand : MonoBehaviour {
+
+    private List<GameObject> handHiden = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+        handHiden.Add(GameObject.Find("MinimalHand(Clone)"));
+        handHiden.Add(GameObject.Find("RigidRoundHand(Clone)/middle"));
+        handHiden.Add(GameObject.Find("RigidRoundHand(Clone)/pinky"));
+        handHiden.Add(GameObject.Find("RigidRoundHand(Clone)/ring"));
+        handHiden.Add(GameObject.Find("RigidRoundHand(Clone)/palm"));
+        handHiden.Add(GameObject.Find("RigidRoundHand(Clone)/forearm"));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        GetHandHiden();
+        // 在UI时，Hand只剩下index（食指）
+        foreach (GameObject obj in handHiden)
+            SetActiveFalse(obj);
+    }
+
+    void GetHandHiden()
+    {
+        handHiden.Add(GameObject.Find("MinimalHand(Clone)"));
+        handHiden.Add(GameObject.Find("RigidRoundHand(Clone)/middle"));
+        handHiden.Add(GameObject.Find("RigidRoundHand(Clone)/pinky"));
+        handHiden.Add(GameObject.Find("RigidRoundHand(Clone)/ring"));
+        handHiden.Add(GameObject.Find("RigidRoundHand(Clone)/palm"));
+        handHiden.Add(GameObject.Find("RigidRoundHand(Clone)/forearm"));
+    }
+
+    void SetActiveFalse(GameObject obj)
+    {
+        if (obj != null)
+            obj.SetActive(false);
+    }
+}
